af3422ac78c7ec36c18ff98f238c54f0f3b8ccb3
Niko <greece93@gmail.com>
2016-11-16 15:13:39 +0100

    ugly functional Commit

--- VaultyOpener/Form1.cs
+++ VaultyOpener/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,54 @@ namespace VaultyOpener
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputPath = textBox1.Text;
+            string outputPath = textBox2.Text;
+
+            string[] filePaths = Directory.GetFiles(inputPath, "*.vdata", SearchOption.TopDirectoryOnly);
+
+            int fileNr = 0;
+            foreach (string filePath in filePaths)
+            {
+                var firstLine = File.ReadAllLines(filePath)[0];
+
+                if (firstLine.Contains("obscure"))
+                {
+                    string filePathOut = outputPath + "\\pic" + fileNr.ToString() + ".png";
+
+                    FileStream inFs = new FileStream(filePath, FileMode.Open);
+                    try
+                    {
+                        FileStream outFs = new FileStream(filePathOut, FileMode.CreateNew);
+                        int hexIn;
+
+                        for (int i = 0; i < 8; i++)
+                        {
+                            inFs.ReadByte();
+                        }
+
+                        for (int i = 0; (hexIn = inFs.ReadByte()) != -1; i++)
+                        {
+                            outFs.WriteByte((byte) hexIn);
+                        }
+
+                        inFs.Close();
+                        outFs.Close();
+
+                        fileNr++;
+                    } catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+            }
+
+
+            MessageBox.Show("Converted " + fileNr.ToString() + " Files");
         }
     }
 }
